1ac752b9294f38a3f881a67c2bf155042b3de732
panjian6 <jiangyun.pan@mail.utoronto.ca>
2019-06-12 16:46:11 -0400

    16th add comment for Tuple

--- Assignment/src/Main/Tuple.java
+++ Assignment/src/Main/Tuple.java
@@ -6,6 +6,7 @@ class Tuple {
     Days dates;
 
     Tuple(Book book, Days date) {
+        //this create a tuple of book and the date information of the book
         this.book = book;
         this.dates = date;
     }
